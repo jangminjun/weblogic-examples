27b05d1357f7317a488489ef6bb26bf1a8197762
jangminjun <chocolariver@naver.com>
2026-04-15 12:48:29 +0900

    init

--- samples/simpleservlet/src/main/java/com/oracle/weblogic/examples/simple/SimpleServlet.java
+++ samples/simpleservlet/src/main/java/com/oracle/weblogic/examples/simple/SimpleServlet.java
@@ -11,13 +11,12 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/simpleservlet")
+@WebServlet("/simpleservelet")
 public class SimpleServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getOutputStream().println("You just reached the simpleservlet");
+        resp.getOutputStream().println("You just reached the simpleservelet");
 
     }
 }
- 
